2aa2e6560ed5a17adb46dda9480c38d4dc6db198
youssef-Ragab-mohamed <yousefragapp7@gmail.com>
2025-04-22 18:22:27 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using TaskManagement.Application.Interfaces;
 using TaskManagement.Application.Services;
@@ -42,6 +43,7 @@ namespace TaskManagement
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<ITaskService, TaskService>();
             builder.Services.AddScoped<ITaskHistoryService, TaskHistoryService>();
+            builder.Services.AddAutoMapper(typeof(Program));
             builder.Services.AddIdentity<User, ApplicationRole>(options =>
             {
                 // Optional Identity settings
@@ -73,11 +75,44 @@ namespace TaskManagement
         ValidateIssuer = true,
         ValidIssuer = builder.Configuration["JWT:Issuer"],
         ValidateAudience = true,
-        ValidAudiences = builder.Configuration.GetSection("JWT:Audiances").Get<List<string>>(),
+        ValidAudiences = builder.Configuration.GetSection("JWT:Audiences").Get<List<string>>(),
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"])),
         ValidateLifetime = true,
     };
 });
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Task Management API",
+                    Version = "v1"
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' [space] and then your valid token.\nExample: Bearer abc123"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+            });
 
 
 
@@ -92,6 +127,10 @@ namespace TaskManagement
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+           
+
+
             app.UseStaticFiles();
 
             app.UseHttpsRedirection();
@@ -107,4 +146,5 @@ namespace TaskManagement
         }
     }
 }
+
 
